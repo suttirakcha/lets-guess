0465cb1c18183585ae668aba49d43f9ba4c9adbe
MarkSuttirak <suttirak.char@gmail.com>
2023-04-18 23:05:40 +0700

    Update sauces.js

--- src/categories/sauces.js
+++ src/categories/sauces.js
@@ -3,6 +3,6 @@ import "../App.scss";
 import Questions from "../components/questions";
 
 export default function Sauces(){
-    const words = ["Ketchup", "Barbecue Sauce", "Hollandaise", "Mayonnaise", "Soy Sauce", "Fish Sauce", "Espagnole", "Sriracha Sauce", "Chilli Sauce", "Peanut Sauce", "Carbonara", "Wasabi", "Salsa", "Apple Sauce", "Béarnaise Sauce", "Zabaione", "Gremolata", "Bolognese Sauce", "White Sauce", "Cream Cheese", "XO Sauce", "Oyster Sauce", "Peanut Butter", "Teriyaki", "Gochujang", "Gravy", "Pesto", "Tahini Sauce", "Tzatziki", "Ajika", "Andalouse", "Béchamel"];
+    const words = ["Ketchup", "Barbecue Sauce", "Hollandaise", "Mayonnaise", "Soy Sauce", "Fish Sauce", "Espagnole", "Sriracha Sauce", "Chilli Sauce", "Peanut Sauce", "Carbonara", "Wasabi", "Salsa", "Apple Sauce", "Béarnaise Sauce", "Zabaione", "Gremolata", "Bolognese Sauce", "White Sauce", "Cream Cheese", "XO Sauce", "Oyster Sauce", "Peanut Butter", "Teriyaki", "Gochujang", "Gravy", "Pesto", "Tahini Sauce", "Tzatziki", "Ajika", "Andalouse", "Béchamel", "Garlic Sauce", "Mustard"];
     return (<Questions answers={words} category="Sauces" redirect="/sauces"/>)
 }
