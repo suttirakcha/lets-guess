9d10740fee74412f637c46357c10451ba0678523
MarkSuttirak <suttirak.char@gmail.com>
2023-04-22 20:23:56 +0700

    Commit Codes

--- src/categories/desserts.js
+++ src/categories/desserts.js
@@ -3,6 +3,6 @@ import "../App.scss";
 import Questions from "../components/questions";
 
 export default function Desserts(){
-  const words = ["Apple Pie","Croissant","Doughnut","Ice Cream","Chocolate","Apfelstrudel","Cheese","Macaron","Churros","Fondue","Sundae","Mousse","Tiramisu","Cupcake","Mochi","Banoffee","Daifuku","Cake","White Chocolate","Cookies","Pie","Yoghurt","Brownie","Cheesecake","Éclair","Tart","Caramel","Lemon Curd","Meringue","Custard","Lamingtons","Pudding","Cream Brulee","Waffle","Crepe","Muffin","Fudge","Parfait","Marshmellow","Bingsu","Panna Cotta","Candy","Candy Bar","Toffee","Cookie and Cream","Biscuit","Stollen","Egg Tart","Fruit Salad","Chiffon Cake"];
+  const words = ["Apple Pie","Croissant","Doughnut","Ice Cream","Chocolate","Apfelstrudel","Cheese","Macaron","Churros","Fondue","Sundae","Mousse","Tiramisu","Cupcake","Mochi","Banoffee","Daifuku","Cake","White Chocolate","Cookies","Pie","Yoghurt","Brownie","Cheesecake","Éclair","Tart","Caramel","Lemon Curd","Meringue","Custard","Lamingtons","Pudding","Cream Brulee","Waffle","Crepe","Muffin","Fudge","Parfait","Marshmellow","Bingsu","Panna Cotta","Candy","Candy Bar","Toffee","Cookie and Cream","Biscuit","Stollen","Egg Tart","Fruit Salad","Chiffon Cake","Mille-feuille"];
   return (<Questions answers={words} category="Desserts" redirect="/desserts"/>)
 }
--- src/categories/drinks.js
+++ src/categories/drinks.js
@@ -3,6 +3,6 @@ import "../App.scss";
 import Questions from "../components/questions";
 
 export default function Drinks(){
-  const words = ["Water","Tea","Coffee","Bubble Tea","Cappuchino","Americano","Milk","Milkshake","Soda","Apfelschorle","Beer","Wine","White Beer","Thai Tea","Mocha","Espresso","Frappuchino","Mojito","Orange Juice","Juice","Apple Juice","Martini","Whiskey","Cocktail","Spritz","Smoothie","Kombucha","Punch","Margarita","Tequila","Lemonade","Oolong Tea","Diabolo","Almond Milk","Soju","Baekseju","Sbiten","Coconut Milk","Soy Milk","Brisk","Hemp Milk","Kopi","Aojiru","Malted Milk","Milk Tea","Latte","Champagne","Negroni","Energy Drink","Lassi","Masala Chai"];
+  const words = ["Water","Tea","Coffee","Bubble Tea","Cappuchino","Americano","Milk","Milkshake","Soda","Apfelschorle","Beer","Wine","White Beer","Thai Tea","Mocha","Espresso","Frappuchino","Mojito","Orange Juice","Juice","Apple Juice","Martini","Whiskey","Cocktail","Spritz","Smoothie","Kombucha","Punch","Margarita","Tequila","Lemonade","Oolong Tea","Diabolo","Almond Milk","Soju","Baekseju","Sbiten","Coconut Milk","Soy Milk","Brisk","Hemp Milk","Kopi","Aojiru","Malted Milk","Milk Tea","Latte","Champagne","Negroni","Energy Drink","Lassi","Masala Chai","Earl Grey Tea","Teh Tarik","Black Tea","White Tea"];
   return (<Questions answers={words} category="Drinks" redirect="/drinks"/>)
 }
