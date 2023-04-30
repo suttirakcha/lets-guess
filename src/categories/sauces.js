56a237153c2d84beebaa430a81fd1edc0a19aa06
MarkSuttirak <suttirak.char@gmail.com>
2023-04-30 22:51:09 +0700

    Commit Codes

--- src/categories/sauces.js
+++ src/categories/sauces.js
@@ -3,6 +3,6 @@ import "../App.scss";
 import Questions from "../components/questions";
 
 export default function Sauces(){
-  const words = ["Ketchup","Barbecue Sauce","Hollandaise","Mayonnaise","Soy Sauce","Fish Sauce","Espagnole","Sriracha Sauce","Chilli Sauce","Peanut Sauce","Carbonara","Wasabi","Salsa","Apple Sauce","Béarnaise Sauce","Zabaione","Gremolata","Bolognese Sauce","White Sauce","Cream Cheese","XO Sauce","Oyster Sauce","Peanut Butter","Teriyaki","Gochujang","Gravy","Pesto","Tahini Sauce","Tzatziki","Ajika","Andalouse","Béchamel","Garlic Sauce","Mustard","Thousand Island","Sour Cream","Honey Mustard","Ranch Sauce","Worcestershire Sauce","Chimichurri","Tartar Sauce","Bordelaise Sauce","Gremolata","Romesco","Harissa","Remoulade","Peppercorn Sauce","Aioli","Mornay Sauce","Syrup"];
+  const words = ["Ketchup","Barbecue Sauce","Hollandaise","Mayonnaise","Soy Sauce","Fish Sauce","Espagnole","Sriracha Sauce","Chilli Sauce","Peanut Sauce","Carbonara","Wasabi","Salsa","Apple Sauce","Béarnaise Sauce","Zabaione","Gremolata","Bolognese Sauce","White Sauce","Cream Cheese","XO Sauce","Oyster Sauce","Peanut Butter","Teriyaki","Gochujang","Gravy","Pesto","Tahini Sauce","Tzatziki","Ajika","Andalouse","Béchamel","Garlic Sauce","Mustard","Thousand Island","Sour Cream","Honey Mustard","Ranch Sauce","Worcestershire Sauce","Chimichurri","Tartar Sauce","Bordelaise Sauce","Gremolata","Romesco","Harissa","Remoulade","Peppercorn Sauce","Aioli","Mornay Sauce","Syrup","Chutney","Tamarind Sauce"];
   return (<Questions answers={words} category="Sauces" redirect="/sauces"/>)
 }
