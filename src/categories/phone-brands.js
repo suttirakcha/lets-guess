1b999330ea033698713d7f326995f76583b64bf8
MarkSuttirak <suttirak.char@gmail.com>
2023-05-02 23:35:01 +0700

    Update phone-brands.js

--- src/categories/phone-brands.js
+++ src/categories/phone-brands.js
@@ -3,6 +3,6 @@ import "../App.scss";
 import Questions from "../components/questions";
 
 export default function PhoneBrands(){
-  const words = ["Nokia","Huawei","Oppo","Samsung","Apple","Sony","Honor","Xiaomi","Acer","Asus","BlackBerry","Google","LG","Motorola","Lenovo","Vivo","Realme","Fujitsu","Panasonic","I-Mobile","Microsoft","HTC","Infinix","HP","NEC","Casio","ZTE","Hitachi"];
+  const words = ["Nokia","Huawei","Oppo","Samsung","Apple","Sony","Honor","Xiaomi","Acer","Asus","BlackBerry","Google","LG","Motorola","Lenovo","Vivo","Realme","Fujitsu","Panasonic","I-Mobile","Microsoft","HTC","Infinix","HP","NEC","Casio","ZTE","Hitachi","Sharp","AIS","DTAC","TRUE","Wellcom","Alcatel","Bittium","Smartisan","OnePlus","Meizu","Haier"];
   return (<Questions answers={words} category="Phone Brands" redirect="/phone-brands"/>)
 }
