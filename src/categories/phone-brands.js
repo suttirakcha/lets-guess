670fe02e757f9a98ca23b666cea622db51fe195d
MarkSuttirak <suttirak.char@gmail.com>
2023-05-02 23:28:22 +0700

    Update phone-brands.js

--- src/categories/phone-brands.js
+++ src/categories/phone-brands.js
@@ -3,6 +3,6 @@ import "../App.scss";
 import Questions from "../components/questions";
 
 export default function PhoneBrands(){
-  const words = ["Nokia","Huawei","Oppo","Samsung","Apple","Sony","Honor","Xiaomi","Acer","Asus","BlackBerry","Google","LG","Motorola","Lenovo","Vivo","Realme","Fujitsu","Panasonic","I-Mobile","Microsoft"];
+  const words = ["Nokia","Huawei","Oppo","Samsung","Apple","Sony","Honor","Xiaomi","Acer","Asus","BlackBerry","Google","LG","Motorola","Lenovo","Vivo","Realme","Fujitsu","Panasonic","I-Mobile","Microsoft","HTC","Infinix","HP","NEC","Casio","ZTE","Hitachi"];
   return (<Questions answers={words} category="Phone Brands" redirect="/phone-brands"/>)
 }
