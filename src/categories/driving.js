26b5c0bc0f97cf0ae3a792975f34ff9bfaf2b2a6
MarkSuttirak <suttirak.char@gmail.com>
2023-05-06 22:54:34 +0700

    Update driving.js

--- src/categories/driving.js
+++ src/categories/driving.js
@@ -3,6 +3,6 @@ import "../App.scss";
 import Questions from "../components/questions";
 
 export default function Driving(){
-  const words = ["Motorway","Road","Street","Car","Bus","Lorry","Truck","Traffic","Traffic jam","Traffic lights","Bus stop","Van","Driving licence","Speed","Patrol","Patrol station","Insurance","Accident","Fuel","Hill","Tyre","Engine","Tollway","Bridge"];
+  const words = ["Motorway","Road","Street","Car","Bus","Lorry","Truck","Traffic","Traffic jam","Traffic lights","Bus stop","Van","Driving licence","Speed","Patrol","Patrol station","Insurance","Accident","Fuel","Hill","Tyre","Engine","Tollway","Bridge","Driver","Passenger","Taxi","Zebra crossing","Lane","Two-way traffic","Drainage ditch","U-turn","Parking lot","Building"];
   return (<Questions answers={words} category="Driving" redirect="/driving"/>)
 }
