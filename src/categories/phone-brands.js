373aa760e56f50065b8e40a648ca4cf450ecbc22
MarkSuttirak <suttirak.char@gmail.com>
2023-05-07 19:41:34 +0700

    Commit Codes

--- src/categories/phone-brands.js
+++ src/categories/phone-brands.js
@@ -3,6 +3,6 @@ import "../App.scss";
 import Questions from "../components/questions";
 
 export default function PhoneBrands(){
-  const words = ["Nokia","Huawei","Oppo","Samsung","Apple","Sony","Honor","Xiaomi","Acer","Asus","BlackBerry","Google","LG","Motorola","Lenovo","Vivo","Realme","Fujitsu","Panasonic","I-Mobile","Microsoft","HTC","Infinix","HP","NEC","Casio","ZTE","Hitachi","Sharp","AIS","DTAC","TRUE","Wellcom","Alcatel Mobile","Bittium","Smartisan","OnePlus","Meizu","Haier","Cherry Mobile","Brondi","Wiko","SHIFT","MobiWire","Kyocera"];
+  const words = ["Nokia","Huawei","Oppo","Samsung","Apple","Sony","Honor","Xiaomi","Acer","Asus","BlackBerry","Google","LG","Motorola","Lenovo","Vivo","Realme","Fujitsu","Panasonic","I-Mobile","Microsoft","HTC","Infinix","HP","NEC","Casio","ZTE","Hitachi","Sharp","AIS","DTAC","TRUE","Wellcom","Alcatel Mobile","Bittium","Smartisan","OnePlus","Meizu","Haier","Cherry Mobile","Brondi","Wiko","SHIFT","MobiWire","Kyocera","Archos","Tecno Mobile"];
   return (<Questions answers={words} category="Phone Brands" redirect="/phone-brands"/>)
 }
