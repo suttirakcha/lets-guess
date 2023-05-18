7f6d7b06f0463b272359b43066f9bd0f8747ff1a
MarkSuttirak <suttirak.char@gmail.com>
2023-05-19 00:26:10 +0700

    Commit Codes

--- src/lao/categories/animals.js
+++ src/lao/categories/animals.js
@@ -3,6 +3,6 @@ import "../../App.scss";
 import QuestionsLao from "../components/questions";
 
 export default function AnimalsLao(){
-  const words = ["ໝາ","ແມວ","ນົກ","ກົບ","ງູ","ເສືອ","ສິງໂຕ","ຍຸງ","ເສືອດຳ","ແມງມຸມ","ປາ","ງົວ","ໝູ","ໄກ່","ແກະ","ແພະ","ກວາງ","ຊ້າງ","ລິງ","ໜູ","ມ້າ","ມ້າລາຍ"];
+  const words = ["ໝາ","ແມວ","ນົກ","ກົບ","ງູ","ເສືອ","ສິງໂຕ","ຍຸງ","ເສືອດຳ","ແມງມຸມ","ປາ","ງົວ","ໝູ","ໄກ່","ແກະ","ແພະ","ກວາງ","ຊ້າງ","ລິງ","ໜູ","ມ້າ","ມ້າລາຍ","ແມງໄມ້"];
   return (<QuestionsLao answers={words} category="ສັດຕ່າງໆ" redirect="/lo/animals"/>)
 }
