9d19a44a1bf60d83d65ab0872da79b8a1807f6c2
MarkSuttirak <suttirak.char@gmail.com>
2023-08-12 22:09:04 +0700

    Commit Codes

--- src/categories/social-media.js
+++ src/categories/social-media.js
@@ -3,6 +3,6 @@ import "../App.scss";
 import Questions from "../components/questions";
 
 export default function SocialMedia(){
-  const words = ["Facebook","Instagram","YouTube","Flickr","LinkedIn","Pinterest","TikTok","WeChat","Line","BeeTalk","WhatsApp","Friendster","Google+","Weibo","QQ","Telegram","Snapchat","Kuaishou","Qzone","Twitter","Reddit","Quora","Discord","Slack","Twitch","Tumblr","Mastodon","hi5","KakaoTalk","Likee","Viber"];
+  const words = ["Facebook","Instagram","YouTube","Flickr","LinkedIn","Pinterest","TikTok","WeChat","Line","BeeTalk","WhatsApp","Friendster","Google+","Weibo","QQ","Telegram","Snapchat","Kuaishou","Qzone","X (Twitter)","Reddit","Quora","Discord","Slack","Twitch","Tumblr","Mastodon","hi5","KakaoTalk","Likee","Viber"];
   return (<Questions answers={words} category="Social Media" redirect="/social-media"/>)
 }
--- src/lao/categories/desserts.js
+++ src/lao/categories/desserts.js
@@ -3,6 +3,6 @@ import "../../App.scss";
 import QuestionsLao from "../components/questions";
 
 export default function DessertsLao(){
-  const words = ["ເຄັກ","ກະແລັມ","ມາກາຣອນ","ພາຍ","ຊັອກໂກແລັດ","ເຄັກນົມສົດ","ເຂົ້າຈີ່","ພາຍໝາກນັດ","ເຄັກໝາກກ້ຽມ","ຄຸກກີ","ເຂົ້າໜົມອຶ່ງ","ເຄບ","ບິງຊູ","ໂລຕີ"];
+  const words = ["ເຄັກ","ກະແລັມ","ມາກາຣອນ","ພາຍ","ຊັອກໂກແລັດ","ເຄັກນົມສົດ","ເຂົ້າຈີ່","ພາຍໝາກນັດ","ເຄັກໝາກກ້ຽມ","ຄຸກກີ","ເຂົ້າໜົມອຶ່ງ","ເຄບ","ບິງຊູ"];
   return (<QuestionsLao answers={words} category="ເຂົ້າໜມຫວານ" redirect="/lo/desserts"/>)
 }
