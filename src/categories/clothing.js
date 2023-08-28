f60de922d248acb6f8064515fde9477b8cec2916
MarkSuttirak <suttirak.char@gmail.com>
2023-08-28 23:43:47 +0700

    Commit Codes

--- src/categories/clothing.js
+++ src/categories/clothing.js
@@ -3,6 +3,6 @@ import "../App.scss";
 import Questions from "../components/questions";
 
 export default function Clothing(){
-  const words = ["Shirt","Trousers","Skirt","Scarf","Shoes","Socks","Bra","Gloves","Hat","Glasses","Mask","Necklace","Earring","Wristband","Neck tie","Gown","Sari","Qi Pao","Hanbok","Hijab","Jeans","Underwear","Belt","Pyjaman","Jacket","Kimono","Ring","Jumper","Raincoat","Headscarf","Tights","Dress","Coat","Trainer Shoes","Swimsuit"];
+  const words = ["Shirt","Trousers","Skirt","Scarf","Shoes","Socks","Bra","Gloves","Hat","Glasses","Mask","Necklace","Earring","Wristband","Neck tie","Gown","Sari","Qi Pao","Hanbok","Hijab","Jeans","Underwear","Belt","Pyjaman","Jacket","Kimono","Ring","Jumper","Raincoat","Headscarf","Tights","Dress","Coat","Trainer Shoes","Swimsuit","Bodysuit"];
   return (<Questions answers={words} category="Clothing" redirect="/clothing"/>)
 }
