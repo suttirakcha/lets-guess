0cd0168d5f4112883dcb79b4f9c16fef7546b7db
MarkSuttirak <suttirak.char@gmail.com>
2023-10-15 22:09:31 +0700

    Update categories.js

--- src/lao/components/categories.js
+++ src/lao/components/categories.js
@@ -1,36 +1,15 @@
 export const cateLists = {
   eatingLo: [
-    {
-      category: "ເຂົ້າໜົມຫວານ",
-      link: "/lo/desserts"
-    },
-    {
-      category: "ເຄື່ອງດື່ມ",
-      link: "/lo/drinks"
-    },
-    {
-      category: "ອາຫານ",
-      link: "/lo/foods"
-    },
+    {category: "ເຂົ້າໜົມຫວານ",link: "/lo/desserts"},
+    {category: "ເຄື່ອງດື່ມ",link: "/lo/drinks"},
+    {category: "ອາຫານ",link: "/lo/foods"},
   ],
   geographyLo: [
-    {
-      category: "ແຂວງໃນປະເທດລາວ",
-      link: "/lo/laos-provinces"
-    },
-    {
-      category: "ສະຖານທີ່",
-      link: "/lo/places"
-    }
+    {category: "ແຂວງໃນປະເທດລາວ",link: "/lo/laos-provinces"},
+    {category: "ສະຖານທີ່",link: "/lo/places"}
   ],
   otherLo: [
-    {
-      category: "ເຄື່ອງຄົວ",
-      link: "/lo/kitchenware"
-    },
-    {
-      category: "ສັດຕ່າງໆ",
-      link: "/lo/animals"
-    },
+    {category: "ເຄື່ອງຄົວ",link: "/lo/kitchenware"},
+    {category: "ສັດຕ່າງໆ",link: "/lo/animals"},
   ],
 }
